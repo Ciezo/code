d35692412c30ebf538456f5d01a473adc1c69dea
Cloyd Van <abcd123.letters@gmail.com>
2022-02-22 08:56:21 +0800

    Book and some essentials to python

---
+++ python/py_essentials/essentials.py
@@ -0,0 +1,66 @@
+##############################################
+# @author Cloyd Van S. Secuya 
+# Date: Febuary 22, 2022
+##############################################
+
+# In this journey, I will go again from basics to indermediate up to advanced. I am pretty much familiar with Python language
+# But I thought that I can create this project so that I can rehone my current skills to mastery. 
+# So far, I encountered Python with programming assignments and problems as well as using it for Data Science particularly 
+# NumPy, Pandas, Scilearn kit, and more into creating and training Models.
+# 
+# 
+# I want to learn and understand more because I know I can do it, and I am confident that I can do so.  
+
+##############################################
+# G O A L S 
+#   - To create and develop programs 
+#   - To design and think of algorithms
+#   - To create a GUI app 
+#   - To create a database, SQL and Python
+# YOU CAN DO THIS CLOYD! I CAN DO THIS!
+##############################################
+
+#
+# Python is a programming language used in mamy ways one of which is creating programs, applications, and scripts to 
+# run or build tasks
+# 
+# With Python we can create GUI by using their modules built for that. Moreover, we can also create programs to create and 
+# insert values to databases.  
+#
+# There are many more things we can do with Python just like with other programming languages. It is up to us to utilize all 
+# of Python's resources and modules to suit our needs 
+
+##############################################
+# References: 
+# https://www.w3schools.com/python/
+# Python For Everybody (Book) 
+##############################################
+
+##############################################
+# Essentials in Python 
+# What I know and seem to be familiar so far 
+# I am also familiar and used to the Java language
+##############################################
+# Basics
+# _________________________
+# Variables 
+# Expressions 
+# Statements 
+# Conditions 
+# Loops 
+# Boolean 
+# Arrays 
+# Lists 
+# Tuple 
+# Dictionary 
+# File reading 
+# File writing 
+# Functions 
+
+# Intermediate 
+# _________________________
+# OOP (Classes, Abstraction, Polymorphism, Inheritance, Encapsulation)
+# Event Driven Approach
+# Event handling 
+# Threading
+# GUI
