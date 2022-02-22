a6b441d7e72369bf683440d09243b542cd49bb09
Cloyd Van <abcd123.letters@gmail.com>
2022-02-22 09:15:32 +0800

    Basics numeric, character, string  variables declaration in Python

---
+++ python/basics/variables.py
@@ -0,0 +1,27 @@
+###############################################
+# @author Cloyd Van S. Secuya 
+# Date: Febuary 22, 2022
+##############################################
+
+# Declaring a variable in Python is very easy, we do not really need to declare its data type before naming it
+# Unlike in othe programming languages like C, C++, and Java, where we declare a data type for each variable. 
+# In Python, we simply assign a value into a variable, and the interpreter knows what type it is.  
+
+# Common Data Types 
+# Integer, Long, Float, Double, Boolean, String, Character
+
+# Declaring a variable of numeric types 
+num_int = 1 
+print(num_int)
+
+num_float = 1.50 
+print(num_float)
+
+# Declaring a variable of String 
+message = "Hello, World!"
+print(message)
+
+# Declaring a variable of character 
+# NOTE: Characters contain only one literal character value
+character = 'E'
+print(character)
